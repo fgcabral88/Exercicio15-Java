874abc54202bc3ed42182d9301394b3dd02fa258
fgcabral88 <fgcabral88@gmail.com>
2020-04-08 18:13:35 -0300

    Projeto Exercicio 15 criado

---
+++ Funcao02/src/classes/Fatorial.java
@@ -0,0 +1,35 @@
+
+package classes;
+
+public class Fatorial {
+    
+    // Atributos:
+    private int num = 0;
+    private int fat = 1;
+    private String formula = "";
+    
+    // Método setValor:
+   public void setValor(int n){
+        num = n;
+        int f = 1;
+        String s = "";
+ 
+        for (int c = n; c > 1; c--) {
+            f *= c;
+            s += c + " x ";
+        }
+        s += "1 =";
+        fat = f;
+        formula = s;
+    }
+    
+    // Método getFatorial:
+    public int getFatorial(){
+        return fat;
+    }
+    
+    // Método getFormula:
+    public String getFormula(){
+        return formula;
+    }
+}
